48e2eb9e87fcb59142f650830cde7229a7eaf56d
Andrey Dyldin <and@cesbo.com>
2022-11-24 15:59:52 +0200

    move theme switch to main-menu

--- src/docs.js
+++ src/docs.js
@@ -15,6 +15,18 @@ window.ListboxDragenter = function(ev) {
 window.ListboxDragleave = function(ev) {
 }
 
+window.ToggleTheme = function(ev) {
+    const el = document.documentElement
+    if(el.classList.contains('light')) {
+        el.classList.remove('light')
+        ev.target.innerText = 'Light'
+    } else {
+        el.classList.add('light')
+        ev.target.innerText = 'Dark'
+    }
+    ev.target.blur()
+}
+
 function renderSource(el) {
     const lines = el
         .innerHTML
